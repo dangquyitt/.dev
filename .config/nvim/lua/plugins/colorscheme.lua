47f9e8ce3d8e55681d30a7063219ca63b1044acd
dangquyitt <dangquyitt@gmail.com>
2026-08-21 20:35:18 +0700

    refactor: reorganize configuration files and add keymaps and options

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,11 +4,12 @@ return {
         name = "catppuccin",
         priority = 1000,
         lazy = false,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "macchiato",
-            })
-            vim.cmd.colorscheme("catppuccin")
+        opts = {
+            flavour = "macchiato",
+        },
+        config = function(_, opts)
+            require("catppuccin").setup(opts)
+            vim.cmd.colorscheme "catppuccin-nvim"
         end,
     }
 }
